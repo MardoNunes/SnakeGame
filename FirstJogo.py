d4ff4ecb4f65873bd6d5973acd1f85edd303857f
Mardoqueu Freire Nunes <mardonunes@gmail.com>
2022-06-18 00:38:43 -0300

    Limite de tela adicionado

--- FirstJogo.py
+++ FirstJogo.py
@@ -26,18 +26,18 @@ y = altura / 2
 x_verde = random.randint(40, 600)
 y_verde = random.randint(50, 430)
 
-#Aqui ciramos a variavel relogio para criar um frame padrão para a movimentação do jogo
+#Aqui criamos a variavel relogio para criar um frame padrão para a movimentação do jogo
 relogio = pygame.time.Clock()
 
-#Iremos criar nossa fonte para colocarmo um msg na tela e um contador de pontos 
+#Iremos criar nossa fonte para colocarmos uma msg na tela e um contador de pontos 
 fonte = pygame.font.SysFont('gargi', 20, True, True)
 pontos = 0
 
 #Agr iremos criar o loop principal
 #A cada segundo o jogo é atualizado, por isso um loop infinito
-#todo o script do jogo dece estar nesse loop
+#todo o script do jogo deve estar nesse loop
 while True:
-    relogio.tick(144) #Aqui adicionamos o valor do framerate do jogo, no caso 144
+    relogio.tick(30) #Aqui adicionamos o valor do framerate do jogo, no caso 60
     tela.fill((0, 0, 0)) #"repinta a tela de preto para apagar o rastro do retalngulo"
     mensagem = f'Pontos: {pontos}' #aqui temo a nossa msg que ira ser atualizada a cada loop
     texto_formatado = fonte.render(mensagem, True, (255, 255, 255)) #aqui renderizamos a fonte criada acima
@@ -46,7 +46,7 @@ while True:
             pygame.quit()
             exit() #chamada de funcao para fechar janela
 
-        #Adicionando movimento ao objt apratir do telcado
+        #Adicionando movimento ao objt apartir do teclado
         if pygame.key.get_pressed()[K_a]:
             x = x - 20
         if pygame.key.get_pressed()[K_d]:
@@ -55,14 +55,26 @@ while True:
             y = y + 20
         if pygame.key.get_pressed()[K_w]:
             y = y - 20
+
+        #comando para fazer com que o objt n saia da tela
+        if (x == 0):
+            x = 20
+        if (x == 640):
+            x = 620
+        if (y == 0):
+            y = 20
+        if (y == 480):
+            y = 440
     #Esses proximos comandos irão desenhar algo na tela
     ret_vermelho = pygame.draw.rect(tela, (255, 0, 0), (x, y, 40, 50))
     ret_verde = pygame.draw.rect(tela, (0, 255, 0), (x_verde, y_verde, 40, 50))
 
+    #condicoa de colisao e contador de pontos
     if ret_vermelho.colliderect(ret_verde):
         x_verde = random.randint(40, 600)
         y_verde = random.randint(50, 430)
         pontos += 1 
+        
 
    #Agr para que a msg realmente apareca na tela, iremos usar o comando seguinte
     tela.blit(texto_formatado, (480, 35))
