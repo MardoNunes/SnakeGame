e86e3f6fa6deb26b68d8d0d26d3913cb74750eef
Mardoqueu Freire Nunes <mardonunes@gmail.com>
2022-06-25 23:58:10 -0300

    Criando o corpo da cobra

--- FirstJogo.py
+++ FirstJogo.py
@@ -20,11 +20,11 @@ tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Primeiro Jogo!')
 
 #variaveis usadas para que a posição do meu objt seja no centro
-x = int(largura / 2)
-y = int(altura / 2)
+x_cobra = int(largura / 2)
+y_cobra = int(altura / 2)
 
-x_verde = random.randint(40, 600)
-y_verde = random.randint(50, 430)
+x_maca = random.randint(40, 600)
+y_maca = random.randint(50, 430)
 
 #Aqui criamos a variavel relogio para criar um frame padrão para a movimentação do jogo
 relogio = pygame.time.Clock()
@@ -42,14 +42,22 @@ pygame.mixer.music.play()
 som_colisao = pygame.mixer.Sound('smw_1-up.wav')
 som_colisao.set_volume(1) #volume do som de colisao
 
+#Criando o Corpinho da cobra
+lista_cobra = [] #essa lista guarda os valores por onde passa, incrementação dela no loop
+
+def aumenta_cobra(lista_cobra):
+    for XeY in lista_cobra:
+        pygame.draw.rect(tela, (0, 255, 0), (XeY[0], XeY[1], 20, 20))
+
+
 #Agr iremos criar o loop principal
 #A cada segundo o jogo é atualizado, por isso um loop infinito
 #todo o script do jogo deve estar nesse loop
 while True:
     relogio.tick(30) #Aqui adicionamos o valor do framerate do jogo, no caso 60
-    tela.fill((0, 0, 0)) #"repinta a tela de preto para apagar o rastro do retalngulo"
+    tela.fill((255, 255, 255)) #"repinta a tela de preto para apagar o rastro do retalngulo"
     mensagem = f'Pontos: {pontos}' #aqui temo a nossa msg que ira ser atualizada a cada loop
-    texto_formatado = fonte.render(mensagem, True, (255, 255, 255)) #aqui renderizamos a fonte criada acima
+    texto_formatado = fonte.render(mensagem, True, (0, 0, 0)) #aqui renderizamos a fonte criada acima
     for event in pygame.event.get(): # cehca se há algum evento
         if event.type == QUIT:
             pygame.quit()
@@ -57,34 +65,35 @@ while True:
 
         #Adicionando movimento ao objt apartir do teclado
         if pygame.key.get_pressed()[K_a]:
-            x = x - 20
+            x_cobra = x_cobra - 10
         if pygame.key.get_pressed()[K_d]:
-            x = x + 20
+            x_cobra = x_cobra + 10
         if pygame.key.get_pressed()[K_s]:
-            y = y + 20
+            y_cobra = y_cobra + 10
         if pygame.key.get_pressed()[K_w]:
-            y = y - 20
+            y_cobra = y_cobra - 10
 
         #comando para fazer com que o objt n saia da tela
-        if (x == 0):
-            x = 20
-        if (x == 640):
-            x = 620
-        if (y == 0):
-            y = 20
-        if (y == 480):
-            y = 440
-    #Esses proximos comandos irão desenhar algo na tela
-    ret_vermelho = pygame.draw.rect(tela, (255, 0, 0), (x, y, 40, 50))
-    ret_verde = pygame.draw.rect(tela, (0, 255, 0), (x_verde, y_verde, 40, 50))
+
+
+
+  #Esses proximos comandos irão desenhar algo na tela
+    cobra = pygame.draw.rect(tela, (0, 255, 0), (x_cobra, y_cobra, 20, 20))
+    maca = pygame.draw.rect(tela, (255, 0, 0), (x_maca, y_maca, 20, 20))
 
     #condicoa de colisao e contador de pontos
-    if ret_vermelho.colliderect(ret_verde):
-        x_verde = random.randint(40, 600)
-        y_verde = random.randint(50, 430)
+    if cobra.colliderect(maca):
+        x_maca = random.randint(40, 600)
+        y_maca = random.randint(50, 430)
         pontos += 1 
         som_colisao.play()
-        
+
+    #Criando a cobrinha
+    lista_cabeca = []
+    lista_cabeca.append(x_cobra)
+    lista_cabeca.append(y_cobra)
+    lista_cobra.append(lista_cabeca)
+    aumenta_cobra(lista_cobra)
 
    #Agr para que a msg realmente apareca na tela, iremos usar o comando seguinte
     tela.blit(texto_formatado, (480, 35))
